f3a927b9663c412de6cf16d3d3911f15a179978a
zcmgyu <zcmgyu@gmail.com>
2026-02-11 23:56:50 +0700

    feat: set default and fallback tool

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Reexport the native module. On web, it will be resolved to ReactNativePencilKitModule.web.ts
 // and on native platforms to ReactNativePencilKitModule.ts
 export { PencilKitView } from './ReactNativePencilKitView';
-export * from  './ReactNativePencilKit.types';
+export * from './ReactNativePencilKit.types';
